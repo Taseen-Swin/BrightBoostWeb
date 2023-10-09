9d2318bbf46e15bd3de8e0c32a41c0848238dec8
Korn <104338734@student.swin.edu.au>
2023-10-09 18:29:46 +1100

    Add All current on process pages and test push to github

---
+++ src/components/CenteredTab.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { Link } from 'react-router-dom';
+
+
+
+export default function CenteredTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Login" to='/StudentLogin' LinkComponent={Link} value="/StudentLogin" />
+        <Tab label="Sign Up" to='/StudentSignup' LinkComponent={Link} value="/StudentSignup" />    
+      </Tabs>
+    </Box>
+  );
+}
